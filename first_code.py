04b06729db4834d84849c0de7f03a50e014f169b
James <luojames52@gmail.com>
2021-11-08 14:23:34 -0800

    fib

--- first_code.py
+++ first_code.py
@@ -1,4 +1,14 @@
 #hello world
 def main():
     print("Hello World")
+#fibonacci recursion
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
 main()
+print(fib(10))
